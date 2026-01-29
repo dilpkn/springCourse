994bd9f93b4bec53de7b58939f9a4cacd5553531
Diana Lipkina <fpm.lipkina@bsu.by>
2026-01-29 15:32:09 +0300

    Add files via upload

---
+++ springcourse7/springcourse2/src/main/java/org/example/ClassicalMusic.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ClassicalMusic implements Music {
+    @Override
+    public String getSong(){
+        return "Hungarian Rhapsodes";
+    }
+
+}
