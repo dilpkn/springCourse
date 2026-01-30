35674ed1df5ee0b15da768dd35208b999fb77426
Diana Lipkina <fpm.lipkina@bsu.by>
2026-01-30 13:02:22 +0300

    Add files via upload

---
+++ springcourse11/springcourse2/src/main/java/org/example/MusicPlayer.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+//@Component
+public class MusicPlayer {
+
+    @Value("${musicPlayer.name}")
+    private String name;
+
+    @Value("${musicPlayer.volume}")
+    private int volume;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    private Music music;
+    private Music music2;
+
+//    @Autowired
+        public MusicPlayer(@Qualifier("rockMusic") Music music, @Qualifier("classicalMusic") Music music2) {
+        this.music = music;
+        this.music2 = music2;
+    }
+
+    public String playMusic() {
+        return "Playing: " + music.getSong() + ", "+ music2.getSong();
+    }
+}
