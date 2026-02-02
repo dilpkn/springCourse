b174d2a60dec5a3f96445e3eb946d9e8b85d4aa9
Diana Lipkina <fpm.lipkina@bsu.by>
2026-02-03 02:32:34 +0300

    Add files via upload

---
+++ _SPRING MVC/mvc10/src/main/java/org/example/config/MySpringMvcDispatcherServletInitialazer.java
@@ -0,0 +1,37 @@
+package org.example.config;
+
+import org.springframework.web.filter.HiddenHttpMethodFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
+public class MySpringMvcDispatcherServletInitialazer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {SpringConfig.class}; // знает где спринг конфигурация
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"}; //посылаем запросы на деспетчер сервлет
+    }
+
+    //для фильтра для patch, delete
+    @Override
+    public void onStartup(ServletContext aServletContext) throws ServletException {
+        super.onStartup(aServletContext);
+        registerHiddenFieldFilter(aServletContext);
+    }
+
+    private void registerHiddenFieldFilter(ServletContext aContext) {
+        aContext.addFilter("hiddenHttpMethodFilter",
+                new HiddenHttpMethodFilter()).addMappingForUrlPatterns(null ,true, "/*");
+    }
+}
---
+++ _SPRING MVC/mvc10/src/main/java/org/example/dao/PersonDAO.java
@@ -0,0 +1,45 @@
+package org.example.dao;
+
+import org.example.models.Person;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private static int PEOPLE_COUNT;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<Person>();
+
+        people.add(new Person(++PEOPLE_COUNT,"Tom"));
+        people.add(new Person(++PEOPLE_COUNT,"Anya"));
+        people.add(new Person(++PEOPLE_COUNT,"Sam"));
+        people.add(new Person(++PEOPLE_COUNT,"Alisa"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id){
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+
+    public void save(Person person){
+        person.setId(++PEOPLE_COUNT);
+        people.add(person);
+    }
+
+    public void update(int id, Person updatePerson){
+        Person personToBeUpdated = show(id);
+        personToBeUpdated.setName(updatePerson.getName());
+    }
+
+    public void delete(int id){
+        people.removeIf(person -> person.getId() == id);
+    }
+
+}
